68d0d6b8369ecf8d9133fe7aada688460fdd3f02
davi <aryconstrucoesepremoldados@gmail.com>
2025-07-28 15:16:28 -0300

    fix: re-adiciona função cn para corrigir erros de build

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,10 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 // Funções utilitárias para geração de PDFs
 // Versão simplificada e robusta
 
